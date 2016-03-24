be4ef4cae11da2d0d8e7f7212e4cc0151ca63374
StrongSad <jacobkirkbarrus@gmail.com>
2016-03-23 17:18:33 -0700

    finished the first two questions

--- google_shopping.js
+++ google_shopping.js
@@ -1,5 +1,32 @@
 var data = require("./products.json")
 
 // Write your solutions below
+// console.log(data["items"]);
 
-console.log(data["items"]);
+
+/*Question one*/
+
+
+// var count = 0;
+// for(var i = 0; i < data['items'].length; i ++) {
+// 	if(data['items'][i].kind === `shopping#product`) {
+// 		count++;
+// 	}
+// }
+
+// console.log(count);
+
+
+/*Question Two*/
+
+
+
+for(var i = 0; i < data['items'].length; i++) {
+	var inventories = data['items'][i].product.inventories;
+	for(var j = 0; j < inventories.length; j++) {
+		if(inventories[j].availability === 'backorder') {
+			console.log(inventories[j]);
+		}
+	}
+}
+
